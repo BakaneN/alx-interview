5a2f371a7a0ba36c77f9c9479d2d0be41c684bf0
BakaneN <mizbkn@gmail.com>
2025-06-26 15:38:33 +0000

    Permissions updated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,9 +1,38 @@
+#!/usr/bin/python3
+"""
+Prime Game - Maria and Ben take turns choosing primes and removing
+multiples from a set of integers from 1 to n. The player unable to
+make a move loses the round.
+"""
+
+
 def isWinner(x, nums):
-    if x < 1 or not nums:
-        return None
+    """
+    Determines the winner of each round of the Prime Game.
+    Maria always plays first.
+
+    Args:
+        x (int): number of rounds
+        nums (list): list of integers for each round
+
+    Returns:
+        str or None: name of the player with the most wins, or None if tied
+    """
 
     def count_primes(n):
-        primes = [True] * (n + 1)
+        """
+        Counts the number of prime numbers up to n using the Sieve of Eratosthenes
+
+        Args:
+            n (int): upper limit to find primes
+
+        Returns:
+            int: count of prime numbers between 1 and n
+        """
+        if n < 2:
+            return 0
+
+        primes = [True for _ in range(n + 1)]
         primes[0] = primes[1] = False
 
         for i in range(2, int(n ** 0.5) + 1):
@@ -13,12 +42,15 @@ def isWinner(x, nums):
 
         return sum(primes)
 
+    if not nums or x < 1:
+        return None
+
     maria = 0
     ben = 0
 
     for n in nums:
         prime_moves = count_primes(n)
-
+        # If odd, Maria wins. If even, Ben wins.
         if prime_moves % 2 == 1:
             maria += 1
         else:
@@ -30,3 +62,4 @@ def isWinner(x, nums):
         return "Ben"
     else:
         return None
+
